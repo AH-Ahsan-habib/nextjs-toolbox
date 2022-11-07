6206a101edf3b863562e5935f6a43f79fb22636e
Ah Ahsan Habib <106630569+AH-Ahsan-habib@users.noreply.github.com>
2022-11-08 02:44:13 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFile%3AFacebook_f_logo_(2021).svg&psig=AOvVaw1MkmpV01HVYADFqWUkg8tz&ust=1667940224645000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOjp5Nb3nPsCFQAAAAAdAAAAABAE" alt="Facebook" className={styles.logo} /> for you
       </footer>
     </>
   )
