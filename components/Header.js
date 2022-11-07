aa915ea8a5c1be66d4a9a41b4c50fa1581bd4faf
Ah Ahsan Habib <106630569+AH-Ahsan-habib@users.noreply.github.com>
2022-11-08 02:49:24 +0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
 export default function Header({ title }) {
   return (
     <>
-      <h1 className="title">{title}</h1>
-        <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="">
+      <h1 className="title">Facebook</h1>
+       
           
           
     </>
